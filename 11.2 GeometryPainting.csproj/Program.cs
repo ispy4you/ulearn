577ded86d7404c874c0a375bdc02723ddd5dfffb
Ivan <44336741+chuzhmaroff@users.noreply.github.com>
2019-05-13 02:16:18 +0500

    Add files via upload

---
+++ 11.2 GeometryPainting.csproj/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+using GeometryTasks;
+
+namespace GeometryPainting
+{
+    internal static class Program
+    {
+        private static List<Segment> CreateSegments()
+        {
+            var result = new List<Segment>();
+            for (var i = 0; i <= 255; i++)
+            {
+                var segment = new Segment
+                {
+                    Begin = new Vector {X = 0, Y = i},
+                    End = new Vector {X = 255, Y = i}
+                };
+                if (i != 0) segment.SetColor(Color.FromArgb(i, i, i));
+                result.Add(segment);
+            }
+
+            return result;
+        }
+
+        private static void DrawSegments(object sender, PaintEventArgs e)
+        {
+            var segments = CreateSegments();
+            foreach (var segment in segments)
+            {
+                Pen pen = null;
+                pen = new Pen(segment.GetColor());
+                e.Graphics.DrawLine(pen, (float) segment.Begin.X, (float) segment.Begin.Y, (float) segment.End.X,
+                    (float) segment.End.Y);
+            }
+        }
+
+
+        /// <summary>
+        ///     The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        private static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            var form = new Form();
+            form.ClientSize = new Size(255, 255);
+            form.Paint += DrawSegments;
+            form.FormBorderStyle = FormBorderStyle.FixedDialog;
+            form.MaximizeBox = false;
+            Application.Run(form);
+        }
+    }
+}
---
+++ 11.2 GeometryPainting.csproj/SegmentExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Drawing;
+using System.Threading.Tasks;
+using GeometryTasks;
+
+namespace GeometryPainting
+{
+    public static class SegmentExtensions
+    {
+        static Dictionary<Segment, Color> dictionary = new Dictionary<Segment, Color>();
+
+        public static Color GetColor(this Segment segment)
+        {
+            if (!dictionary.ContainsKey(segment))
+                return Color.Black;
+            return dictionary[segment];
+        }
+
+        public static void SetColor(this Segment segment, Color color)
+        {
+            if (!dictionary.ContainsKey(segment))
+                dictionary.Add(segment, color);
+            else dictionary[segment] = color;
+        }
+    }
+}
