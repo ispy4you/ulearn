ecabe0642ae94b475fe0d5e0bb9fcc74e1c82a00
Ivan <44336741+chuzhmaroff@users.noreply.github.com>
2019-05-13 01:48:38 +0500

    Add files via upload

---
+++ 4.1 Names.csproj/HeatmapTask.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace Names
+{
+    internal static class HeatmapTask
+    {
+        public static HeatmapData GetBirthsPerDateHeatmap(NameData[] names)
+        {
+            var lenghtDays = 30;
+            var minValueDays = 2;
+            var lenghtMonths = 12;
+            var minMonth = 1;
+            var days = new string[lenghtDays];
+            for (int i = 0; i < lenghtDays; i++)
+                days[i] = (i + minValueDays).ToString();
+            var months = new string[lenghtMonths];
+            for (int i = 0; i < lenghtMonths; i++)
+                months[i] = (i + minMonth).ToString();
+            var heatMap = new double[30, 12];
+            foreach (var calendar in names)
+                if (calendar.BirthDate.Day != 1)
+                    heatMap[calendar.BirthDate.Day - 2, calendar.BirthDate.Month - 1]++;
+            return new HeatmapData("Пример карты интенсивностей", heatMap, days, months);
+        }
+    }
+}
---
+++ 4.1 Names.csproj/HistogramTask.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace Names
+{
+    internal static class HistogramTask
+    {
+        public static HistogramData GetBirthsPerDayHistogram(NameData[] names, string name)
+        {
+            var years = new string[31];
+            for (var y = 0; y < 31; y++)
+                years[y] = (y + 1).ToString();
+            var birthsCounts = new double[31];
+            foreach (var simpleName in names)
+            {
+                if (simpleName.Name == name)
+                    if (simpleName.BirthDate.Day != 1)
+                        birthsCounts[simpleName.BirthDate.Day - 1]++;
+            }
+            return new HistogramData(string.Format("Рождаемость людей с именем '{0}'", name), years, birthsCounts);
+        }
+    }
+}
